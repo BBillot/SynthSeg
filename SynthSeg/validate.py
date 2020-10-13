3f4ce0c2baf529dbd7a73944b705bedcfec8af6b
BBillot <benjamin.billot1993@gmail.com>
2020-10-13 18:25:22 +0100

    added validation options

--- SynthSeg/validate.py
+++ SynthSeg/validate.py
@@ -19,11 +19,14 @@ def validate_training(image_dir,
                       validation_main_dir,
                       path_label_list,
                       step_eval=1,
+                      aff_ref='FS',
+                      sigma_smoothing=0,
+                      keep_biggest_component=False,
                       cropping=None,
                       conv_size=3,
                       n_levels=5,
                       nb_conv_per_level=2,
-                      feat_multiplier=1,
+                      feat_multiplier=2,
                       activation='elu',
                       recompute=True):
     """This function validates models saved at different epochs of the same training.
@@ -36,6 +39,10 @@ def validate_training(image_dir,
     :param validation_main_dir: path of the folder where all the models validation subfolders will be saved.
     :param path_label_list: path of the numpy array containing all the label values to validate on.
     :param step_eval: (optional) If step_eval > 1 skips models when validating, by validating on models step_eval apart.
+    :param aff_ref: (optional) affine matrix with which the models were trained. Can be 'FS' (default), or 'identity.
+    :param sigma_smoothing: (optional) If not None, the posteriors are smoothed with a gaussian kernel of the specified
+    standard deviation.
+    :param keep_biggest_component: (optional) whether to only keep the biggest component in the predicted segmentation.
     :param cropping: (optional) whether to crop the input to smaller size while being run through the network.
     The result is then given in the original image space. Can be an int, a sequence, or a 1d numpy array.
     :param n_levels: (optional) number of level for the Unet. Default is 5.
@@ -62,9 +69,10 @@ def validate_training(image_dir,
         if (not os.path.isfile(dice_path)) | recompute:
             predict(path_images=image_dir,
                     path_segmentations=model_val_dir,
-                    path_model=path_model,
-                    segmentation_label_list=path_label_list,
                     cropping=cropping,
+                    aff_ref=aff_ref,
+                    sigma_smoothing=sigma_smoothing,
+                    keep_biggest_component=keep_biggest_component,
                     conv_size=conv_size,
                     n_levels=n_levels,
                     nb_conv_per_level=nb_conv_per_level,
