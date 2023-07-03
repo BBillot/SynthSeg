27009e2fa5af84b9d9ef92f7dfd8f15c2022776b
Patrick Scheibe <patrick@halirutan.de>
2023-07-03 17:56:56 +0200

    Fix brain generation. Need to test it with config still

--- scripts/slurm/1-generation.py
+++ scripts/slurm/1-generation.py
@@ -3,9 +3,11 @@ from typing import Optional
 from simple_parsing import ArgumentParser
 from os import access, R_OK
 from os.path import isfile
+import logging
+import sys
 
 from SynthSeg.brain_generator_options import GeneratorOptions
-from SynthSeg.brain_generator import BrainGenerator
+from SynthSeg.brain_generator import create_brain_generator
 from ext.lab2im import utils
 
 
@@ -22,6 +24,14 @@ class Options:
 
 
 if __name__ == '__main__':
+    logger = logging.getLogger("Generate Brain")
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
     parser = ArgumentParser()
     parser.add_arguments(Options, "general")
     parser.add_arguments(GeneratorOptions, "generate")
@@ -30,15 +40,30 @@ if __name__ == '__main__':
     general_params: Options = args.general
     if isinstance(general_params.output_dir, str):
         utils.mkdir(general_params.output_dir)
+        utils.mkdir(f"{general_params.output_dir}/images")
+        utils.mkdir(f"{general_params.output_dir}/labels")
+    else:
+        logger.error(f"No output directory specified")
+        exit(-1)
 
     conf_file = general_params.config_file
     if isinstance(conf_file, str) and isfile(conf_file) and access(conf_file, R_OK):
-        print("Loading generator config from configuration file.")
+        logger.info("Loading generator config from configuration file.")
         generator_config = GeneratorOptions.load(conf_file)
     else:
-        print("No valid config file. Initialize generator with default values and cmd-line parameters.")
+        logger.info("No valid config file. Initialize generator with default values and cmd-line parameters.")
         generator_config = args.generate
 
-    print(generator_config)
+    if general_params.count <= 0:
+        logger.error(f"Number of training pairs to generate must be positive but was {general_params.count}.")
+        exit(0)
 
-    # TODO: Create brain generator from this config. Maybe we can merge cmg args into a loaded config
+    generator = create_brain_generator(generator_config)
+    image_output_dir = f"{general_params.output_dir}/images"
+    label_output_dir = f"{general_params.output_dir}/labels"
+
+    for i in range(general_params.count):
+        image, label = generator.generate_brain()
+        utils.save_volume(image, generator.aff, generator.header, f"{image_output_dir}/{str(i).zfill(6)}.nii.gz")
+        utils.save_volume(label, generator.aff, generator.header, f"{label_output_dir}/{str(i).zfill(6)}.nii.gz")
+        logger.info(f"Exported training pair number {str(i).zfill(6)}")
