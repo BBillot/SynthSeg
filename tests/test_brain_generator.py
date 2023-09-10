80e153aac294638476dfd05213c282ddf1e9953b
David Carreto Fidalgo <david.carreto.fidalgo@mpcdf.mpg.de>
2023-09-10 11:26:38 +0200

    add test

--- tests/test_brain_generator.py
+++ tests/test_brain_generator.py
@@ -3,6 +3,8 @@ from . import TestData
 import nibabel as nib
 import numpy as np
 import SynthSeg.brain_generator as bg
+import timeit
+import pytest
 
 
 def test_brain_generator(fixed_random_seed):
@@ -11,8 +13,13 @@ def test_brain_generator(fixed_random_seed):
     im, lab = brain_generator.generate_brain()
     if TestData.debug_nifti_output:
         img_data = tf.squeeze(im)
-        nib.save(nib.Nifti1Image(img_data.numpy(), np.eye(4)), TestData.get_test_output_dir() / "generated_brain.nii")
-    assert im.shape == lab.shape, "Shape of the label image and the generated MRI are not the same"
+        nib.save(
+            nib.Nifti1Image(img_data.numpy(), np.eye(4)),
+            TestData.get_test_output_dir() / "generated_brain.nii",
+        )
+    assert (
+        im.shape == lab.shape
+    ), "Shape of the label image and the generated MRI are not the same"
 
 
 def test_tfrecords(tmp_path):
@@ -44,4 +51,35 @@ def test_tfrecords_compression(tmp_path):
     brain_generator.generate_tfrecord(tfrecord, compression_type="GZIP")
     size2 = tfrecord.stat().st_size
 
-    assert size1/size2 > 40
+    assert size1 / size2 > 40
+
+
+@pytest.mark.parametrize("compression", ("", "GZIP"))
+def test_read_tfrecords(tmp_path, compression):
+    def measure_iteration(ds):
+        def func():
+            for _ in ds:
+                pass
+
+        return func
+
+    label_map_files = TestData.get_label_maps()
+    brain_generator = bg.BrainGenerator(label_map_files[0], target_res=8, batchsize=3)
+
+    tf.keras.utils.set_random_seed(43)
+    for i in range(2):
+        tfrecord = tmp_path / f"test{i}.tfrecord"
+        brain_generator.generate_tfrecord(tfrecord, compression_type=compression)
+    files = list(tmp_path.glob("*.tfrecord"))
+
+    dataset = bg.read_tfrecords(files, compression_type=compression)
+    time1 = timeit.timeit(measure_iteration(dataset), number=10)
+
+    dataset = bg.read_tfrecords(
+        files,
+        num_parallel_reads=2,
+        compression_type=compression,
+    )
+    time2 = timeit.timeit(measure_iteration(dataset), number=10)
+
+    assert time1 > time2
